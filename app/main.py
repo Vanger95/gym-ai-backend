71ab782137714900b16e985f4bfd66a4d75be856
Vanger95 <vinurawathmal471@gmail.com>
2026-08-17 23:03:10 +0100

    Allow production frontend origin

--- app/main.py
+++ app/main.py
@@ -31,6 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://white-mushroom-01aa6c60f.7.azurestaticapps.net",
     ],
     allow_credentials=True,
     allow_methods=["*"],
